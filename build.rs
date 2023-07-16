0a0a2cb81bb4f1afd3cb3b3214fcd79460ab6a4f
Jerico Alcaras <jalcaras@microsoft.com>
2023-07-16 03:43:53 +0100

    Wrap in gRPC service.

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
-fn main() {
-    cxx_build::bridge("src/main.rs")
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    cxx_build::bridge("src/server.rs")
         .file("src/native.cc")
         .compile("rust-cxx-stuff");
+    tonic_build::compile_protos("protos/greet.proto")?;
+    Ok(())
 }
